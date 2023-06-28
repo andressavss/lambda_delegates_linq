cb115b522eafa631d210a8f4797cfbcf74418769
Andressa Santos <andressavieiradsantos@gmail.com>
2023-06-28 00:04:48 -0300

    Update Comparison<T>

--- Comparison/Comparison/Program.cs
+++ Comparison/Comparison/Program.cs
@@ -12,7 +12,9 @@ class Program
         list.Add(new Product("Notebook", 1200.00));
         list.Add(new Product("Tablet", 450.00));
 
-        list.Sort(CompareProducts);
+        Comparison<Product> comp = CompareProducts;
+
+        list.Sort(comp);
 
         foreach (Product p in list)
         {
