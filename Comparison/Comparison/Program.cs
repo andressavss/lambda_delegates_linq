76fec80de92f3b378f5c21d0adb3676821972c46
Andressa Santos <andressavieiradsantos@gmail.com>
2023-06-28 00:09:22 -0300

    Update with Lambda

--- Comparison/Comparison/Program.cs
+++ Comparison/Comparison/Program.cs
@@ -12,7 +12,7 @@ class Program
         list.Add(new Product("Notebook", 1200.00));
         list.Add(new Product("Tablet", 450.00));
 
-        Comparison<Product> comp = CompareProducts;
+        Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 
         list.Sort(comp);
 
@@ -20,9 +20,5 @@ class Program
         {
             Console.WriteLine(p);
         }
-    }
-    static int CompareProducts(Product p1, Product p2)
-    {
-        return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
     }
 }
