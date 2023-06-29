af850e0bc5a5f8f5dc14357e1e095e1fce5cdbf7
Andressa Santos <andressavieiradsantos@gmail.com>
2023-06-29 19:16:10 -0300

    LINQ with operations

---
+++ LINQOperations/LINQOperations/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace LINQOperations.Entities
+{
+    internal class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ID}, {Name}, ${Price.ToString("F2", CultureInfo.InvariantCulture)}, {Category.Name}, {Category.Tier}";
+        }
+    }
+}
