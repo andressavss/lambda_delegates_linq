a903df49016d6496285d5cf91426be8aaf37799e
Andressa Santos <andressavieiradsantos@gmail.com>
2023-06-29 13:07:45 -0300

    First commit

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Delegates.Services;
+
+namespace Delegates;
+delegate double BinaryNumericOperation(double n1, double n2);
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = 10;
+        double b = 12;
+
+        // BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+        BinaryNumericOperation op = CalculationService.Max;
+
+        // double result = op(a, b);
+        double result = op.Invoke(a, b);
+        Console.WriteLine(result);
+    }
+}
