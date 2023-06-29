988a53cc86a7c68eacbc3a5027aaa68f327d0819
Andressa Santos <andressavieiradsantos@gmail.com>
2023-06-29 13:15:22 -0300

    Update with multicast delegates

--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -2,7 +2,7 @@
 using Delegates.Services;
 
 namespace Delegates;
-delegate double BinaryNumericOperation(double n1, double n2);
+delegate void BinaryNumericOperation(double n1, double n2);
 class Program
 {
     static void Main(string[] args)
@@ -10,11 +10,9 @@ class Program
         double a = 10;
         double b = 12;
 
-        // BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
-        BinaryNumericOperation op = CalculationService.Max;
-
-        // double result = op(a, b);
-        double result = op.Invoke(a, b);
-        Console.WriteLine(result);
+        BinaryNumericOperation op = CalculationService.ShowMax;
+        op += CalculationService.ShowSum;
+        
+        op(a, b);
     }
 }
