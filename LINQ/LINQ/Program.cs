ec1192609875c92afbc5fa7bc744893b160f6177
Andressa Santos <andressavieiradsantos@gmail.com>
2023-06-29 16:07:29 -0300

    Preseting the LINQ

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Specific the data source
+        int[] numbers = new int[] { 2, 3, 4, 5 };
+
+        // Define the query expression
+        IEnumerable<int> result = numbers.Where(x => x % 2 == 0)
+            .Select(x => x * 10);
+
+        // Execute the query
+        foreach (int x in result)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
