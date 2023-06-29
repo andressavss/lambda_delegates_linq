7ab5f529c7f2b44a7834d95e99f68a5eeef65a95
Andressa Santos <andressavieiradsantos@gmail.com>
2023-06-29 19:50:34 -0300

    SQL like syntax

---
+++ LINQOperations2/LINQOperations2/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQOperations2.Entities
+{
+    internal class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
---
+++ LINQOperations2/LINQOperations2/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace LINQOperations2.Entities
+{
+    internal class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ID}, {Name}, ${Price.ToString("F2", CultureInfo.InvariantCulture)}, {Category.Name}, {Category.Tier}";
+        }
+    }
+}
